b9e1ef97342abab31b7e33bf826547025ed2ffec
Emanuele Rocca <ema@linux.it>
2012-08-21 19:13:32 +0200

    Certificates creation through the OpenSSL module

--- manager.py
+++ manager.py
@@ -1,4 +1,7 @@
 import os
+import random
+
+from OpenSSL import crypto
 
 import common
 common.extend_path()
@@ -6,15 +9,88 @@ common.extend_path()
 from conpaas.core.controller import Controller
 from conpaas.core.misc import file_get_contents
 
+def gen_rsa_keypair():
+    pkey = crypto.PKey()
+    pkey.generate_key(crypto.TYPE_RSA, 2048)
+    return pkey
+    
+def create_x509_req(req_key, uid, sid, org, email, cn, role):
+    req = crypto.X509Req()
+    subj = req.get_subject()
+
+    subj.O = org
+    subj.CN = cn
+    subj.emailAddress = email
+    subj.userId = uid
+    subj.serviceLocator = sid
+    subj.role = role
+
+    req.set_pubkey(req_key)
+    req.sign(req_key, "md5")
+    return req
+
+def create_x509_cert(cert_dir, x509_req):
+    # Load the CA cert
+    ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, 
+        file_get_contents(os.path.join(cert_dir, "ca_cert.pem")))
+    
+    # Load private key
+    key = crypto.load_privatekey(crypto.FILETYPE_PEM, 
+        file_get_contents(os.path.join(cert_dir, "key.pem")))
+
+    # Create new certificate
+    newcert = crypto.X509()
+
+    # Generate serial number
+    serial = random.randint(1, 2048)
+    newcert.set_serial_number(serial)
+    
+    # Valid for one year starting from now 
+    newcert.gmtime_adj_notAfter(60 * 60 * 24 * 365)
+    newcert.gmtime_adj_notBefore(0)
+
+    # Issuer, subject and public key
+    newcert.set_issuer(ca_cert.get_subject())
+    newcert.set_subject(x509_req.get_subject())
+    newcert.set_pubkey(x509_req.get_pubkey())
+
+    # Sign
+    newcert.sign(key, "md5")
+
+    return crypto.dump_certificate(crypto.FILETYPE_PEM, newcert)
+
+def generate_certificate(cert_dir, uid, sid, role, email, cn, org):
+    """Generates a new x509 certificate for a manager from scratch.
+
+    Creates a key, a request and then the certificate."""
+
+    # Get CA cert
+    ca_cert = file_get_contents(os.path.join(cert_dir, "ca_cert.pem"))
+
+    # Generate keypair
+    req_key  = gen_rsa_keypair()
+
+    # Generate certificate request
+    x509_req = create_x509_req(req_key, uid, sid, org, email, cn, role)
+
+    # Sign the request
+    certificate = create_x509_cert(cert_dir, x509_req)
+
+    return { 'ca_cert': ca_cert, 
+             'key': crypto.dump_privatekey(crypto.FILETYPE_PEM, req_key), 
+             'cert': certificate }
+
 class ManagerController(Controller):
 
-    def __get_ca_cert(self):
+    def _get_certificate(self, email, cn, org):
         config_parser = self._Controller__config_parser
 
-        ca_cert_file = os.path.join(config_parser.get('conpaas', 'CERT_DIR'), 
-            "ca_cert.pem")
+        user_id = config_parser.get("manager", "FE_USER_ID")
+        service_id = config_parser.get("manager", "FE_SERVICE_ID")
+        cert_dir = config_parser.get('conpaas', 'CERT_DIR')
 
-        return file_get_contents(ca_cert_file)
+        return generate_certificate(cert_dir, user_id, service_id, 
+                                    "manager", email, cn, org)
 
     def _get_context_file(self, service_name, cloud):
         """Override default _get_context_file. Here we generate the context
@@ -53,7 +129,9 @@ class ManagerController(Controller):
             os.path.join(mngr_cfg_dir, 'default-manager.cfg'))
 
         # Add service-specific config file (if any)
-        mngr_service_cfg = os.path.join(mngr_cfg_dir, service_name + '-manager.cfg')
+        mngr_service_cfg = os.path.join(mngr_cfg_dir, 
+            service_name + '-manager.cfg')
+
         if os.path.isfile(mngr_service_cfg):
             mngr_cfg += file_get_contents(mngr_service_cfg)
 
@@ -79,10 +157,13 @@ class ManagerController(Controller):
                 mngr_startup_scriptname)
 
         # Get key and a certificate from CA
-        #mngr_certs = self._get_certificate()
-        tmpl_values['mngr_certs_cert'] = "foo"
-        tmpl_values['mngr_certs_key'] = "bar"
-        tmpl_values['mngr_certs_ca_cert'] = "foo_bar"
+        mngr_certs = self._get_certificate(email="info@conpaas.eu", 
+                                           cn="ConPaaS", 
+                                           org="Contrail")
+
+        tmpl_values['mngr_certs_cert']    = mngr_certs['cert']
+        tmpl_values['mngr_certs_key']     = mngr_certs['key']
+        tmpl_values['mngr_certs_ca_cert'] = mngr_certs['ca_cert']
         
         # Concatenate the files
         return """%(cloud_script)s
@@ -127,21 +208,21 @@ def start(service_name, service_id, user_id):
     # Create a context file for the specific service
     controller.generate_context(service_name)
 
-if __name__ == "__main__":
-    start("php", "1", "1")
+    # Only useful for testing purposes if the director is not running on a
+    # public IP
+    controller._Controller__deduct_credit = lambda x: True
 
-#def is_dummy(config_parser):
-#    return config_parser.get("iaas", "DRIVER") == "dummy"
-#
-#if True or is_dummy(config_parser):
-#    controller._Controller__deduct_credit = lambda x: True
-#
-## test_manager(ip, port) not implemented yet. Just return True.
-#controller.create_nodes(1, lambda ip, port: True, None)
-#
-#for vm in controller.list_vms().values():
-#    print vm
-#
-#if is_dummy(config_parser):
-#    for reservation_timer in controller._Controller__reservation_map.values():
-#        reservation_timer.stop()
+    # FIXME: test_manager(ip, port) not implemented yet. Just return True.
+    node = controller.create_nodes(1, lambda ip, port: True, None)
+
+    # Stop the reservation timer or the call will not return
+    for reservation_timer in controller._Controller__reservation_map.values():
+        reservation_timer.stop()
+
+    return node
+
+if __name__ == "__main__":
+    print start("php", "1", "1")
+    #cert = generate_certificate(cert_dir="/var/tmp/certs", uid="1", sid="1",
+    # role="manager", email="info@conpaas.eu", cn="ConPaaS", org="Contrail")
+    #print cert['cert']
